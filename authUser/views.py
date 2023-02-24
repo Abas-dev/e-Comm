63d81fda912e3aa01faa6ca869c8bceb3f125079
abas-dev <abasisaac22@gmail.com>
2023-02-24 16:09:34 +0100

    added the usercreation view

--- authUser/views.py
+++ authUser/views.py
@@ -1,10 +1,30 @@
 from django.contrib.auth.views import LoginView,LogoutView
+from django.contrib.auth import login,authenticate  
 from django.views.generic.edit import CreateView
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.shortcuts import redirect
 
 
 class LoginUserView(LoginView):
     template_name = 'login.html'
 
-class CreateUserView(LoginView):
+class CreateUserView(CreateView):
     template_name = 'register.html'
+    form_class = UserCreationForm
+    
+    def post(self, request, *args, **kwargs):
+        username = self.request.POST['username']
+        email = self.request.POST['email']
+        fname = self.request.POST['fName']
+        lname = self.request.POST['lName']
+        password = self.request.POST['password1']
+        passwordConf = self.request.POST['password2']
+        if password != passwordConf:
+            messages.error(request,'password do not match')
+        else:
+            user = User.objects.create_user(username=username,email=email,first_name=fname,last_name=lname,password=password)
+            user.save()
+            return redirect('auth:login')
+        return super().post(request, *args, **kwargs)
